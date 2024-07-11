bb8c23646395a96d51ce8dc051a8a4be7334ff6d
Dens Sumesh <dens.sumesh79@gmail.com>
2024-07-10 20:31:26 -0700

    feature: display created migration files

--- src/commands/generate.rs
+++ src/commands/generate.rs
@@ -13,9 +13,12 @@ pub async fn generate_command(args: GenerateArgs) -> Result<(), CLIError> {
     let migration_dir = migrations_dir.join(format!("{}_{}", Utc::now().timestamp(), args.name));
 
     tokio::fs::create_dir(migration_dir.clone()).await?;
+    let up_file = migration_dir.clone().join("up.sql");
+    let down_file = migration_dir.clone().join("down.sql");
+    tokio::fs::File::create(up_file.clone()).await?;
+    tokio::fs::File::create(down_file.clone()).await?;
 
-    tokio::fs::File::create(migration_dir.clone().join("up.sql")).await?;
-    tokio::fs::File::create(migration_dir.clone().join("down.sql")).await?;
-
+    println!("Up.sql created at: {}", up_file.display());
+    println!("Down.sql created at: {}", down_file.display());
     Ok(())
 }
